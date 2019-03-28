f8f7453e489e710041f8511f624db2dd4ba105cc
Alex Frech <frechfrech2@gmail.com>
2019-03-28 16:48:57 -0600

    add name to init

--- helper_functions/__init__.py
+++ helper_functions/__init__.py
@@ -1,11 +1,1 @@
-name = 'frechfrechfrech_first_package'
-# def joke():
-#     return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-#             u'Beiherhund das Oder die Flipperwaldt gersput.')
-#
-# def add_2_plus_2():
-#     return '2+2=4'
-#
-# def test2():
-#     print('The auto-update worked!')
-#     return
+name = 'helper_functions'
